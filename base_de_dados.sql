cc3883c79ee4050df504c8d1b54b5cafac33280a
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-01-12 19:59:40 -0300

    consertei sql

--- base_de_dados.sql
+++ base_de_dados.sql
@@ -1,6 +1,6 @@
 CREATE TABLE conta (
   id SERIAL PRIMARY KEY,
-  horario_criacao_conta TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP,
+  horario_criacao_conta TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   usuario VARCHAR(30) NOT NULL,
   senha VARCHAR(255) NOT NULL
 );
@@ -8,7 +8,7 @@ CREATE TABLE conta (
 CREATE TABLE cadastro (
   id SERIAL PRIMARY KEY,
   conta_id INT REFERENCES conta(id) ON DELETE CASCADE,
-  horario_criacao_cadastro TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP,
+  horario_criacao_cadastro TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   titulo VARCHAR(50) NOT NULL,
   descricao TEXT NOT NULL,
   localizacao VARCHAR(255), 
@@ -19,17 +19,15 @@ CREATE TABLE condicao_transferencia (
   cadastro_enviante_id INT REFERENCES cadastro(id) ON DELETE CASCADE,
   valor_condicao_transferencia REAL NOT NULL,
   cadastro_id_a_transferir INT REFERENCES cadastro(id) ON DELETE CASCADE,
-  PRIMARY KEY (cadastro_id, cadastro_id_transferido)
+  PRIMARY KEY (cadastro_enviante_id, cadastro_id_a_transferir)
 );
 
 CREATE TABLE transferencia (
-  horario_transferencia TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP,
-
+  horario_transferencia TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   id SERIAL,
-
-  cadastro_enviante_id REFERENCES cadastro(id) ON DELETE CASCADE,
+  cadastro_enviante_id INT REFERENCES cadastro(id) ON DELETE CASCADE,
   valor_transferido REAL NOT NULL,
-  cadastro_receptor_id REFERENCES cadastro(id) ON DELETE CASCADE,
+  cadastro_receptor_id INT REFERENCES cadastro(id) ON DELETE CASCADE,
   PRIMARY KEY (id, cadastro_enviante_id)
 );
 
@@ -39,14 +37,6 @@ CREATE TABLE periodicidade (
   periodicidade SMALLINT DEFAULT 0 NOT NULL CHECK (periodicidade >= 0), 
   periodos_desde_criacao SMALLINT DEFAULT 0 NOT NULL,
   tipo_periodicidade VARCHAR(6) NOT NULL CHECK (tipo_periodicidade IN ('Dia', 'Semana', 'Mês')),
-  data_inicio TIMESTAMPZ NOT NULL,
-  cadastro_quantidade_mudanca INT DEFAULT 1 NOT NULL
+  data_inicio TIMESTAMP WITH TIME ZONE NOT NULL,
+  periodicidade_quantidade_mudanca INT DEFAULT 1 NOT NULL
 );
-
-INSERT INTO conta (usuario, senha) VALUES
-('fulano', '1234'),
-('beutrano', '4321');
-
-INSERT INTO cadastro (conta_id, necessidade_ou_contribuicao, titulo, descricao, quantidade) VALUES
-(1, 'C', 'par de chinelo', 'par de chinelos tamanho 34 branco seminovo', 1),
-(2, 'N', 'chinelo 34/35', 'olá, busco por um chinelo tamanho 34/35 pra cima, mas não muito mior que isso', 1);

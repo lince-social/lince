e17b4e5de2f21b7f209246d7915d89f42aa57838
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-02-02 21:34:46 -0300

    That thing was too scuffed to be called an app, too shit, too gpted, too amateur, it was more like a large hunk of passion

--- streamlit_crud.py
+++ streamlit_crud.py
@@ -4,11 +4,11 @@ from uuid import uuid4
 import pandas as pd
 
 conn = psycopg2.connect(
-    host="localhost",
-    port="5432",
-    database="personallince",
-    user="postgres",
-    password="atencao")
+    host='localhost',
+    port='5432',
+    database='personallince',
+    user='postgres',
+    password='atencao')
 
 cursor = conn.cursor()
 
@@ -49,36 +49,38 @@ def execute_sql_file(file):
 
 
 table = st.sidebar.radio('Select a table', ['conta', 'cadastro', 'proposta_transferencia', 'sentinela', 'periodicidade'])
-
-
+operation = st.sidebar.radio('Select an operation', ['Insert', 'Update', 'Delete', 'Custom Query', 'SQL File'])
 display_table(table)
 
-operation = st.sidebar.radio("Select an operation", ["Insert", "Update", "Delete",'Custom Query','SQL File'])
-
-if operation == "Insert":
-    values = {}
+if operation == 'Insert':
     df = execute_query(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table}'")
+    values = {}
+    
     for i, row in df.iterrows():
-        col_name = row["column_name"]
-        col_type = row["data_type"]
-        if col_type == "uuid":
+        col_name = row['column_name']
+        col_type = row['data_type']
+        if col_type == 'uuid':
             value = uuid4()
-        elif col_type == "boolean":
+        elif col_type == 'boolean':
             value = st.sidebar.checkbox(col_name)
         else:
             value = st.sidebar.text_input(col_name)
         values[col_name] = value
+    
     values = tuple(values.values())
-    if st.sidebar.button("Insert"):
+    
+    if st.sidebar.button('Insert'):
         insert_record(table, values)
 
-elif operation == "Update":
+elif operation == 'Update':
     values = {}
     df = execute_query(f"SELECT a.attname FROM pg_index i JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey) WHERE i.indrelid = '{table}'::regclass AND i.indisprimary")
+
     pk = df.iloc[0, 0]
     pk_value = st.sidebar.text_input(f"Enter the {pk} of the record to be updated")
-    query = f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table}'"
-    df = execute_query(query)
+    
+    df = execute_query(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table}'")
+    
     for i, row in df.iterrows():
         col_name = row["column_name"]
         col_type = row["data_type"]
@@ -89,15 +91,19 @@ elif operation == "Update":
         else:
             value = st.sidebar.text_input(col_name)
         values[col_name] = value
+    
     set_clause = ", ".join([f"{k} = '{v}'" for k, v in values.items() if v != ""])
     where_clause = f"{pk} = '{pk_value}'"
+    
     if st.sidebar.button("Update"):
         update_record(table, set_clause, where_clause)
 
 elif operation == "Delete":
     df = execute_query(f"SELECT a.attname FROM pg_index i JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey) WHERE i.indrelid = '{table}'::regclass AND i.indisprimary")
+
     pk = df.iloc[0, 0]
     pk_value = st.sidebar.text_input(f"Enter the {pk} of the record to be deleted")
+    
     where_clause = f"{pk} = '{pk_value}'"
 
     if st.sidebar.button("Delete"):
@@ -105,13 +111,15 @@ elif operation == "Delete":
 
 elif operation == "Custom Query":
     text = st.sidebar.text_area("Enter your SQL script:") 
+    
     if st.sidebar.button("Execute Query"):
         result = execute_query(text)
         if result is not None:
             st.sidebar.dataframe(result)
 
-elif operation == "Upload SQL File":
+elif operation == "SQL File":
     uploaded_file = st.sidebar.file_uploader("Choose a .sql file", type="sql")
+    
     if uploaded_file is not None:
         file_contents = uploaded_file.read().decode("utf-8")
         if st.sidebar.button("Execute SQL File"):

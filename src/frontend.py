d802fb4e8b728f183d02169fe265c5a99c81835f
zaviduds <zaviduds@gmail.com>
2024-03-19 16:44:35 -0300

    bugfix intermitent commit

--- src/frontend.py
+++ src/frontend.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 from backend import *
+import psycopg2
+
 
 table = st.sidebar.radio('Select a table', ['conta', 'cadastro', 'proposta_transferencia', 'sentinela', 'periodicidade'])
 operation = st.sidebar.radio('Select an operation', ['Insert', 'Update', 'Delete', 'Custom Query', 'SQL File'])

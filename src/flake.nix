2273951a1a4a875c9dba86e61c9e9ad891f573dc
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-03-18 22:51:20 -0300

    Organização para flakes.nix

---
+++ src/flake.nix
@@ -0,0 +1,17 @@
+{
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+    pythonPackages = pkgs.python3Packages;
+    pyPkgs = with pythonPackages; [ psycopg2 streamlit pandas ];
+  in
+  {
+    devShells.x86_64-linux = {
+      default = pkgs.mkShell {
+        buildInputs = [ (pkgs.python3.withPackages (ps: pyPkgs)) ];
+      };
+    };
+  };
+}

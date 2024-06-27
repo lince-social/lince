c99d4cea9b92b9113fea59ec19836f1364195d20
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-06-27 15:35:04 -0300

    confused_lince restart

--- src/confused_lince.py
+++ src/confused_lince.py
@@ -1,107 +1,192 @@
+import pandas as pd
+from uuid import uuid4
+from os.path import exists
+from datetime import datetime
+import psycopg2
+import subprocess
+
+
+def open_connection()
+    host = 'localhost'
+    port = '5432'
+    database_name = 'lince'
+    user = 'postgres'
+    password = '1'
+
+    conn =  psycopg2.connect(
+        host=host,
+        port=port,
+        database=database_name,
+        user=user,
+        password=password).cursor()
+
+    return conn
+
+
+def execute_query(query):
+    conn.cursor().execute(query)    
+
+    if query.startswith("SELECT"):
+        return pd.Dataframe(conn.cursor().fetchall(), columns=[desc[0] for desc in conn.cursor().description])
+
+    elif query.startswith(("INSERT", "UPDATE", "DELETE")):
+        conn.commit()
+    return None
+
+
+def read_lines():
+    # do stuff
+
+
+def close_connection():
+    conn.close()
+
+
+def question():
+    print('''Choose:
+    [0] Exit
+    [1] Read
+    [2] Add
+    [3] Delete
+    [4] Alter
+    ''')
+    return int(input("Your choice: "))
+
+
+while True:
+    open_connection()
+
+    read_lines()
+    answer = question()
+
+    match question():
+        case 0:
+            break
+        case 1:
+            read_lines()
+        case 2:
+            add_line():
+        case 3:
+            delete_line():
+        case 4:
+            alter_line():
 
 
 
 
 
 
-# import pandas as pd
-# from uuid import uuid4
-# from os.path import exists
-# from datetime import datetime
-# import psycopg2
-# import streamlit as st
-# import subprocess
 
-# host = 'localhost'
-# port = '5432'
-# database_name = 'lince'
-# user = 'postgres'
-# password = 'atencao'
 
-# def check_db_and_populate():
-#     conn = None
-#     try:
-#         conn = psycopg2.connect(
-#             host=host,
-#             port=port,
-#             user=user,
-#             password=password)
-#         cur = conn.cursor()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def check_db_and_populate():
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password)
+        cur = conn.cursor()
         
-#         cur.execute(f"SELECT datname FROM pg_database WHERE datname = '{database_name}'")
-#         result = cur.fetchone()
+        cur.execute(f"SELECT datname FROM pg_database WHERE datname = '{database_name}'")
+        result = cur.fetchone()
         
-#         if result is None:
-#             cur.execute(f"CREATE DATABASE {database_name}")
-#             print(f"Database '{database_name}' created successfully.")
+        if result is None:
+            cur.execute(f"CREATE DATABASE {database_name}")
+            print(f"Database '{database_name}' created successfully.")
             
-#             conn.close()
-#             conn = psycopg2.connect(
-#                 host=host,
-#                 port=port,
-#                 database=database_name,
-#                 user=user,
-#                 password=password
-#             )
+            conn.close()
+            conn = psycopg2.connect(
+                host=host,
+                port=port,
+                database=database_name,
+                user=user,
+                password=password
+            )
             
-#             sql_file_path = 'postgre.sql'
-#             if os.path.exists(sql_file_path):
-#                 with open(sql_file_path, 'r') as file:
-#                     sql_commands = file.read()
-#                 cur.execute(sql_commands)
-#                 conn.commit()
-#                 print(f"Database '{database_name}' populated successfully.")
-#             else:
-#                 print(f"SQL file '{sql_file_path}' not found.")
-#     except OperationalError as e:
-#         print(f"The error '{e}' occurred")
-#     finally:
-#         if conn is not None:
-#             conn.close()
+            if os.path.exists(sql_file_path):
+                with open(sql_file_path, 'r') as file:
+                    sql_commands = file.read()
+                cur.execute(sql_commands)
+                conn.commit()
+                print(f"Database '{database_name}' populated successfully.")
+            else:
+                print(f"SQL file '{sql_file_path}' not found.")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    finally:
+        if conn is not None:
+            conn.close()
 
 
-# def generate_unique_filename(base_filename):
-#     counter = 0
-#     filename = base_filename
-#     while os.path.exists(filename):
-#         counter += 1
-#         filename = f"{base_filename}{counter}.sql"
-#     return filename
+def generate_db_version(base_filename):
+    counter = 0
+    filename = base_filename
+    while os.path.exists(filename):
+        counter += 1
+        filename = f"{base_filename}{counter}.sql"
+    return filename
 
 
-# def execute_query(query):
-#     cursor.execute(query)
-#     if query.strip().upper().startswith('SELECT'):
-#         return pd.DataFrame(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
-#     return None
+def execute_query(query):
+    cursor.execute(query)
+    if query.strip().upper().startswith('SELECT'):
+        return pd.DataFrame(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
+    return None
 
-# def display_table(table):
-#     st.subheader(f'Table: {table}')
+
+def display_table(table):
+    print(f'Table: {table}')
 #     st.dataframe(execute_query(f'SELECT * FROM {table}'))
 
 
-# def insert_record(table, values):
-#     execute_query(f'INSERT INTO {table} VALUES {values}')
-#     conn.commit()
+def insert_record(table, values):
+    execute_query(f'INSERT INTO {table} VALUES {values}')
+    conn.commit()
 
 
-# def update_record(table, set_clause, where_clause):
-#     execute_query(f'UPDATE {table} SET {set_clause} WHERE {where_clause}')
-#     conn.commit()
+def update_record(table, set_clause, where_clause):
+    execute_query(f'UPDATE {table} SET {set_clause} WHERE {where_clause}')
+    conn.commit()
 
 
-# def delete_record(table, where_clause):
-#     execute_query(f'DELETE FROM {table} WHERE {where_clause}')
-#     conn.commit()
+def delete_record(table, where_clause):
+    execute_query(f'DELETE FROM {table} WHERE {where_clause}')
+    conn.commit()
 
 
-# def execute_sql_file(file):
-#     queries = file.split(';')
-#     for query in queries:
-#         query = query.strip()
-#         if query:
-#             execute_query(query)
-#     conn.commit()
+def execute_sql_file(file):
+    queries = file.split(';')
+    for query in queries:
+        query = query.strip()
+        if query:
+            execute_query(query)
+    conn.commit()
 
 
 # def check_and_update_cadastro():
@@ -245,7 +330,7 @@
 
 # elif operation == 'Database to .sql file':
 #     base_filename = 'lincedb'
-#     unique_filename = generate_unique_filename(base_filename)
+    #     unique_filename = generate_db_version(base_filename)
 #     pg_dump_command = [ 'pg_dump', '-U', user, '-W', '-F', 'plain', '-f', unique_filename, database_name ]
 
 #     if st.sidebar.button('Pg_dump database'):
@@ -256,96 +341,4 @@
 #             print(f"Database dump failed with error code {result.returncode}.")    
 
 # else:
-#     st.sidebar.text("Select a valid operation.")
-
-
-
-
-
-
-
-
-# import os
-# from pathlib import Path
-# from datetime import date, datetime
-
-
-# def clean_done_run_scripts_ajust_date():      
-#     with open('~/.vida/tarefas.md', 'r') as recurrence_file:
-#         recurrence_lines = recurrence_file.readlines()
-#     with open('~/.vida/tarefas.md', 'w') as recurrence_file:
-
-#         for recurrence_line in recurrence_lines:
-#             recurrence_line = recurrence_line.strip()
-
-#             if not recurrence_line.startswith('- [x]'): recurrence_file.write(recurrence_line+"\n")
-
-#             if recurrence_line.startswith('- [ ]') and 'every' in recurrence_line:
-#                 recurrence_due_date = datetime.strptime(f"{recurrence_line[-10:]}", "%Y-%m-%d").date()
-#                 # recurrence_
-#                 for root, dirs, files in os.walk('/home/eduardo/.vida/automation/'):
-#                     for file in files:
-#                         if file != os.path.basename(__file__) and Path(file).stem in recurrence_line and (recurrence_due_date < date.today()):
-#                             os.system(f'python {file}')
-
-
-# '''
-# # AGORA
-
-
-# # SEI LA
-# weeks:
-# when done:
-
-# every 2 weeks: weeks 2
-
-# every 2 weeks on fridays: weeks 2 - day
-
-# if today monday, till friday is 4 days, next friday is 4+7 = 11
-
-# days = times * week - (days till next day)
-
-
-# # COMPLETO
-# if has 'every' and date:
-#     if line starts with - [x]:
-
-#             quantity = number after 'every'
-#             if has days:
-#                 dateshift = datetime.shift(days=quantity)
-#             if has months
-#                 dateshift = datetime.shift(months=quantity)
-#             if has years
-#                 dateshift = datetime.shift(years=quantity)
-
-#             if has weeks
-#                 for any day of the week bewteen 'on' and date
-#                     day_of_week = date.today().of_week()
-#                     weekday_numbers = {monday: 0, thursday:1, wednsday:2, tuesday:3, friday:4, saturday:5, sunday:6}
-#                     quantity = 1 + weekday_numbers[date:lowest_value] weekday_numbers[day_of_week:value]
-#                 dateshift = datetime.shift(days=quantity)
-
-
-
-#             date = date + dateshift
-
-#             replace x with space
-#             write line
-
-    
-#     if line starts with - [ ]:
-#             if date is today or less:
-#                 if script == name:
-#                     run script
-#                     if success:
-#                         replace space with x
-# else:
-#     if starts with - [x]
-#         delete line
-#     else:
-#         write line
-
-# '''
-
-# if __name__ == "__main__":
-#     clean_done_run_scripts_ajust_date()
+#     print("Select a valid operation.")

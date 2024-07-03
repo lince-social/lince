65fdb9d28be0058421c2308878d3ecf13861da8f
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-07-03 09:09:20 -0300

    insert.sql updated

--- test/insert.sql
+++ test/insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO conta (usuario, senha) VALUES ('xaviduds', '01234');
+-- INSERT INTO conta (usuario, senha) VALUES ('xaviduds', '01234');
 
-INSERT INTO cadastro (conta_id, titulo, descricao, localizacao, quantidade) VALUES (1, 'Maçã', NULL, NULL, 0);
+INSERT INTO cadastro (id, titulo, descricao, localizacao, quantidade) VALUES (1, 'Maçã', NULL, NULL, 0);

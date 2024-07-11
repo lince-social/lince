c637e3a2ef892e7aed08e4b59772f9a41976d534
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-07-11 12:26:29 -0300

    add postgres stuff for nix-shell, not complete

--- default.nix
+++ default.nix
@@ -12,3 +12,12 @@ in pkgs.mkShell {
   ];
 }
 
+# postgresql = {
+#   authentication = pkgs.lib.mkOverride 10 ''
+#       #type database  DBuser  auth-method
+#       				local all       all     trust
+#     enable = true;
+#       				host all       all     127.0.0.1/32   trust
+#       				host all       all     ::1/128        trust		
+#       				'';
+# };

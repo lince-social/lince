ac09dce72e37170a864578e1b14891a2274f0d08
xaviduds <xaviduds@gmail.com>
2024-07-13 18:43:31 -0300

    refactoring to least abstraction all functions in main hahah

--- src/app/lince.py
+++ src/app/lince.py
@@ -3,243 +3,230 @@ import psycopg2
 import os
 import subprocess
 
-
-def terminal_cleaner():
-    # os.system('clear')
-    return True
-
-
-def lince_header_printer():
-    terminal_cleaner()
-
-    print('- Lince -')
-    
-
-def welcome_printer():
-    lince_header_printer()
-
-    print('\nWelcome to the Lince CLI.')
-
-
+ 
 def create_connection_object(host = 'localhost', user = 'postgres', database = 'lince', password = '1', port = '5432'):
-    connection = psycopg2.connect(
-        host = host,
-        user = user,
-        database = database,
-        password = password,
-        port = port)
-
+    connection = psycopg2.connect( host = host, user = user, database = database, password = password, port = port)
     connection.autocommit = True
+    return connection
+
+
+
+
+def execute_sql_command(command=None, database='lince'):
+    connection = create_connection_object(database=database)
+    cursor = connection.cursor()
+    cursor.execute(command)    
+
+    if command.startswith("SELECT"):
+        df = pd.Dataframe(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
+        connection.close()
+        return df
 
     return connection
 
 
-def exists_db():
-    connection = create_connection_object(database=None)
+
+
+def inform_db_exists_checking():
+    return print('\nChecking if Lince Database exists...')
+def check_exists_db():
+    inform_db_exists_checking()
+
+    connection = psycopg2.connect( host = 'localhost', user = 'postgres', password = '1', port = '5432')
+    connection.autocommit = True
     cursor = connection.cursor()
-
     cursor.execute("SELECT datname FROM pg_database WHERE datname = 'lince'")
-
     return cursor.fetchone()
 
 
+
+
+def inform_db_dumping():
+    return print("\nDatabase lince exists, dumping...")
 def dump_db():
-    print("\nDatabase lince exists, dumping...")
-
-    connection = create_connection_object()
-    cursor = connection.cursor()
-
-    subprocess.run(['pg_dump', '-U' 'postgres', '-W', '-F', 'plain', '-f', '/home/eduardo/lince/src/db/versions/db_dump.sql', 'lince'], text=True, input='1\n')
-
-    connection.close()
-
-    return True
+    inform_db_dumping()
+    return subprocess.run(['pg_dump', '-U' 'postgres', '-W', '-F', 'plain', '-f', '/home/eduardo/lince/src/db/versions/db_dump.sql', 'lince'], text=True, input='1\n')
 
 
-def drop_db():
-    print('\nDump complete, dropping lince database...')
-
-    connection = create_connection_object(database=None)
-    cursor = connection.cursor()
-
-    cursor.execute('DROP DATABASE lince')
-
-    connection.close()
-
-    return True
 
 
+def inform_db_dropping():
+    return print('\nDump complete, dropping lince database...')
+def drop_db(quiet=False):
+    if quiet == False: inform_db_dumping()
+    return execute_sql_command(command='DROP DATABASE lince', database=None)
+
+
+
+
+def inform_db_creation():
+    return print("\nCreating Database Lince...")
 def create_db():
-    print("\nCreating Database Lince...")
-
-    connection = create_connection_object(database=None)
+    inform_db_creation()
+    connection = psycopg2.connect( host = 'localhost', user = 'postgres', password = '1', port = '5432')
+    connection.autocommit = True
     cursor = connection.cursor()
 
     cursor.execute('CREATE DATABASE lince')
 
-    connection.close()
-
+    # execute_sql_command(command='CREATE DATABASE lince', database=None)
     return True
 
 
-def schema_db_chooser():
-    match int(input("\nDatabase Schema Chooser\n\n[0] Lince Schema.\n[1] Other Schema.\n\nYour choice: ")):
+
+
+def inform_db_filling():
+    return('\nLince Database created. Starting filling process...')
+def print_db_schema_options():
+    inform_db_filling()
+    return print("\nDatabase Schema Chooser\n\n[0] Lince Schema.\n[1] Other Schema.")
+def choose_db_schema():
+    print_db_schema_options()
+
+    match int(input("\n\nYour choice: ")):
         case 0:
             return '/home/eduardo/lince/src/db/postgre.sql'
         case 1:
-            return input("Type the .sql file Path: ")
+            sql_file_path = input("Type the .sql file Path: ")
+            if exists(sql_file_path): return sql_file_path
 
-    return None
+            print(f"\nSQL file '{sql_file_path}' not found.")
 
-
+    choose_db_schema()
 def fill_db():
-    connection = create_connection_object()
-    cursor = connection.cursor()
+    with open(choose_db_schema(), 'r') as file: return execute_sql_command(command = file.read())
 
-    sql_file_path = schema_db_chooser()
 
-    if os.path.exists(sql_file_path):
-        with open(sql_file_path, 'r') as file:
-            sql_commands = file.read()
-
-        cursor.execute(sql_commands)
-        print(f"\nDatabase Lince populated successfully.")
-
-    else:
-        print(f"\nSQL file '{sql_file_path}' not found.")
 
 
 def create_and_populate_db_if_not_already_created_or_populated():
-    if exists_db() is not None:
+    if check_exists_db() is not None:
         dump_db()
         drop_db()
-
     create_db()
-    fill_db()
+    return fill_db()
 
-    return True
-   
 
-def app_mode():
-    answer = int(input('''- Lince App -\n\n[0] Auto saver/dumper.\n[1] Save when called for.\n\nSelect mode: '''))
 
-    if isinstance(answer, int) and answer in [0, 1]:
-        return answer
+
+def clean_terminal():
+    return os.system('clear')
+
+
+def print_lince_header():
+    return print('- Lince -')
+    
+
+def choose_app_mode():
+    answer = int(input('''- Lince App -\n\n[0] Autosaver.\n[1] Save when called for.\n\nSelect mode: '''))
+
+    if isinstance(answer, int) and answer in [0, 1]: return answer
 
     print("Wrong type, please type an Integer (int), i.e: 1, 42, 80")
-    app_mode()
+    choose_app_mode()
+def print_app_mode(app_mode):
+    return print('Autosaver ON') if app_mode == 0 else print('Autosaver OFF')
 
 
-def read_lines(table, lines_number):
-    return execute_query('SELECT * FROM {table} LIMIT {lines_number}')
-    
-
-def print_head_cadastro():
-    terminal_cleaner()
-    return read_lines(table='cadastro', lines_number=5)
 
 
-def table_chooser():
-    table = int(input('Select table:\n[0] Exit\n[1] Save\n[2] Cadastro'))
+def read_lines(table, rows):
+    return execute_sql_command('SELECT * FROM {table} LIMIT {lines_number}')
 
-    if isinstance(table, int) and table in [0, 1, 2]:
-        return table
 
-    print('Wrong type or option selected, please enter an Integer (int) from 0 to 2')
-    table_chooser()
-    
 
-def operation_options_printer():
-    options_header = ['[S] Save.', '[E] Exit.'] 
-    operation_options = ['[C] Create.','[R] Read.', '[U] Update.', '[D] Delete.']
+
+def print_cadastro_rows(number):
+    return read_lines(table='cadastro', rows=number)
+
+
+
+
+def print_top_info(app_mode=None):
+    clean_terminal()
+    print_lince_header()
+    print_app_mode(app_mode)
+    print_cadastro_rows(5)
+
+
+
+
+def print_operation_options():
+    options_header = ['[E] Exit.', '[S] Save.'] 
+    operation_options = ['[C] Create.','[R] Read.', '[U] Update.', '[D] Delete.' ]
     table_options = ['[1] Cadastro']
 
-    
+    max_length = max(len(item) for item in (options_header + operation_options + table_options))
+    max_len_list = max(len(operation_options), len(options_header), len(table_options))
 
-    max_element_size = max[(len(item) for item in options_header + operation_options + table_options)] 
+    options_header += [''] * (max_len_list - len(options_header))
+    operation_options += [''] * (max_len_list - len(operation_options))
+    table_options += [''] * (max_len_list - len(table_options))
 
-    return None
+    print('-' * (3 * max_length + 10))
 
+    for h, o, t in zip(options_header, operation_options, table_options):
+        print(f"| {h:{max_length}} | {o:{max_length}} | {t:{max_length}} |")
 
-def operation_chooser():
-    operation_options_printer()
-    choice = int(input('Your Choice: '))
-
-    if isinstance(choice, int) and choice in [0, 1, 2]:
-        return choice
-
-    print('Wrong type or option, please enter an Integer from 0 to 2')
-    operation_chooser()
-    
-
-def execute_operation():
-    print_head_cadastro()
-
-    table = table_chooser()
-    operation = operation_chooser()
-
-  
-   
-                    def add_line(table):
-                        values = input('Add line: ')
-
-                        execute_query(f'INSERT INTO {table} VALUES {values}')
-                        retos.urn None
-
-
-                    def delete_line(table):
-                        where_clause = input('Where clause: ')
-
-                        execute_query(f'DELETE FROM {table} WHERE {where_clause}')
-                        return None
-
-
-                    def update_line(table):
-                        set_clause = input('Set clause: ')
-                        where_clause = input('Where clause: ')
-
-                        execute_query(f'UPDATE {table} SET {set_clause} WHERE {where_clause}')
-                        execute_query(f'')
-                        return None
+    print('-' * (3 * max_length + 10))
     return True
 
 
-def execute_query(query):
-    connection = create_connection_object()
-    connection.cursor().execute(query)    
+def choose_operation():
+    print_operation_options()
+    choice = int(input('Your Choice: '))
 
-    if query.startswith("SELECT"):
-        df = pd.Dataframe(connection.cursor().fetchall(), columns=[desc[0] for desc in connection.cursor().description])
-        connection.close()
-        return df
+    if isinstance(choice, int) and choice in [0, 1, 2]: return choice 
 
-    # elif query.startswith(("INSERT", "UPDATE", "DELETE")):
-        # connection.commit()
+    print('Wrong type or option, please enter an Integer from 0 to 2')
+    choose_operation()
+    
 
-    connection.commit()
-    connection.close()
-    return None
+def execute_operation(operation):
 
+    
+                    # def add_line(table):
+                    #     values = input('Add line: ')
+
+                    #     execute_sql_command(f'INSERT INTO {table} VALUES {values}')
+                    #     retos.urn None
+
+
+                    # def delete_line(table):
+                    #     where_clause = input('Where clause: ')
+
+                    #     execute_sql_command(f'DELETE FROM {table} WHERE {where_clause}')
+                    #     return None
+
+
+                    # def update_line(table):
+                    #     set_clause = input('Set clause: ')
+                    #     where_clause = input('Where clause: ')
+
+                    #     execute_sql_command(f'UPDATE {table} SET {set_clause} WHERE {where_clause}')
+                    #     execute_sql_command(f'')
+                    #     return None
+    return True
+
+
+    
 
 def main():
-    welcome_printer()
-
     create_and_populate_db_if_not_already_created_or_populated()
 
-    match app_mode():
-        case 0:
-            while True:
-                lince_header_printer()
-                print_head_cadastro()
-                execute_operation()
-                dump_db()
-        case 1:
-            while True:
-                lince_header_printer()
-                print_head_cadastro()
-                execute_operation()
-            dump_db
+    app_mode = choose_app_mode()
 
+    print_top_info(app_mode)
+
+    operation = choose_operation()
+
+    while True:
+        execute_operation(operation)
+        if app_mode == 0: dump_db(quiet=True) 
+        operation = choose_operation()
+        print_top_info(app_mode)
+
+    dump_db()
     return False
 
 

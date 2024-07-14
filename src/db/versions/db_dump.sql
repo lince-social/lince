fd5a221f83bc1f409356e65454e79d3aaf04962b
xaviduds <xaviduds@gmail.com>
2024-07-14 00:59:21 -0300

    dump.sql changes early stuff

--- src/db/versions/db_dump.sql
+++ src/db/versions/db_dump.sql
@@ -17,6 +17,91 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: cadastro; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.cadastro (
+    id integer NOT NULL,
+    titulo character varying(50) NOT NULL,
+    descricao text,
+    localizacao character varying(255),
+    quantidade real DEFAULT 0 NOT NULL
+);
+
+
+ALTER TABLE public.cadastro OWNER TO postgres;
+
+--
+-- Name: cadastro_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.cadastro_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.cadastro_id_seq OWNER TO postgres;
+
+--
+-- Name: cadastro_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.cadastro_id_seq OWNED BY public.cadastro.id;
+
+
+--
+-- Name: cadastro id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cadastro ALTER COLUMN id SET DEFAULT nextval('public.cadastro_id_seq'::regclass);
+
+
+--
+-- Data for Name: cadastro; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.cadastro (id, titulo, descricao, localizacao, quantidade) FROM stdin;
+\.
+
+
+--
+-- Name: cadastro_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.cadastro_id_seq', 1, false);
+
+
+--
+-- Name: cadastro cadastro_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cadastro
+    ADD CONSTRAINT cadastro_pkey PRIMARY KEY (id);
+
+
+--
 -- PostgreSQL database dump complete
 --
 

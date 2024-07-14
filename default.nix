648e49b87bec98cdd9c999c1368f0b107d572b3d
xaviduds <xaviduds@gmail.com>
2024-07-14 20:03:31 -0300

    default.nix rm devenv and add packages and config

--- default.nix
+++ default.nix
@@ -2,6 +2,23 @@
   pkgs ? import <nixpkgs> { },
 }:
 pkgs.mkShell {
-  buildInputs = with pkgs; [ devenv ];
-  shellHook = ''test -f `pwd`/devenv.nix && devenv shell || devenv init'';
+  buildInputs = with pkgs; [
+    postgresql
+    git
+    python312
+    python312Packages.python-lsp-server
+    python312Packages.pandas
+    python312Packages.datetime
+    python312Packages.psycopg2
+
+  ];
+  PGDATA = "${toString ./.}/.pg";
+  shellHook = ''
+    pg_ctl stop
+    export PGHOST="$PGDATA"
+
+    [ ! -d $PGDATA ] && pg_ctl initdb -o "-U postgres"
+    pg_ctl -o "-p 5432 -k $PGDATA" start
+    python src/app/lince.py
+  '';
 }

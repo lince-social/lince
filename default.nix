d2abf525a81e7e8b1fdaa2e798ce5e2fd876c3f3
xaviduds <xaviduds@gmail.com>
2024-07-14 16:23:05 -0300

    commit

--- default.nix
+++ default.nix
@@ -3,9 +3,5 @@
 }:
 pkgs.mkShell {
   buildInputs = with pkgs; [ devenv ];
-  # python3
-  # python3Packages.pandas
-  # python3Packages.datetime
-  # python3Packages.psycopg2
   shellHook = ''test -f `pwd`/devenv.nix && devenv shell || devenv init'';
 }
--- devenv.nix
+++ devenv.nix
@@ -5,9 +5,7 @@
   inputs,
   ...
 }:
-
 {
-  # https://devenv.sh/packages/
   packages = with pkgs; [
     git
     python3
@@ -16,19 +14,7 @@
     python3Packages.psycopg2
   ];
 
-  # https://devenv.sh/services/
   services.postgres.enable = true;
 
   enterShell = ''python `pwd`/src/app/lince.py'';
-
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
 }

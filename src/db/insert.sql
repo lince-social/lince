8f12eed25cf4f442bd9f90d0530fd333f04af1e9
xaviduds <xaviduds@gmail.com>
2024-07-19 19:14:35 -0300

    add tasks and roadmap with tags inside the app

--- src/db/insert.sql
+++ src/db/insert.sql
@@ -1,13 +1,16 @@
 insert into record (title, description, quantity) values
-('v0.3.0', 'Conditions (The objective is to have generalized conditions and consequences, so anything can trigger anything else. A periodicity can run a script, a checkpoint can change a quantity through a proportion, etc.)', -1),
+('v0.3.0', 'Conditions (The objective is to have generalized conditions and consequences, so anything can trigger anything else.\n A periodicity can run a script, a checkpoint can change a quantity through a proportion, etc.)', -1),
 ('v0.3.0', 'Checkpoint, i.e. "When a quantity reaches 4"', -1),
 ('v0.3.0', 'Rate, i.e. "When a quantity changes in a certain rate (change/time)"', -1),
 ('v0.3.0', 'Proportion, i.e. "When a quantity changes a certain number"', -1),
 ('v0.3.0', 'Command, i.e. "Shell command, being able to trigger any script in any language, easy to do with nix-shells for dev envs"', -1),
 ('v0.3.0', 'Delta, i.e. "Set a quantity to more or less a number, -1, +4, etc."', -1),
 ('v0.3.0', 'Arquitetura de condições e consequências', -1),
+('v0.3.0', 'Transform frequency into a building block, to be used just like any condition/consequence', -1),
 ('v0.3.0', 'Simulção do efeito do tempo nos cadastros, se possível a partir dos cadastros de todos, coletar esses dados e rodar uma função de pesquisa operacional de simulação, também funciona quando uma transferência foi confirmada mas não efetivada no mundo material, só prometida, aí vai ter o valor atual da quantidade -1 e o previsto caso tudo continue como setado.', -1),
 
+('0.4.0', 'make quantity zero with minimum types of keyboard', -1),
+('0.4.0', 'add help option with explanation of all options and columns info and defaults', -1),
 ('v0.4.0', 'Templates: prov uma manifestação de uso. Pessoas vão modelar sua cadeia produtiva e mostrar ou não, as que mostrarem vão deixar pessoas copiar tudo. Depois o que sobra são trabalhadores se colocando nas tarefas pra levantar tal empreitada.', -1),
 ('v0.4.0', 'Copiar cadastro, seja seu ou de outrem: selecione a opção copiar coisa, e de qual table ai tu digita o where pra puxar o que tu quer editar, se resposta é sim em editar 1 por 1 abre 1 por 1 e passa por todas as colunas de todas as coisas, se é editar com where ele faz um bulk editing', -1),
 ('v0.4.0', 'Criar table config, vai conter as configurações do aplicativo, quando o aplicativo começar ele vai olhar todas as colunas e se comportar conforme as informações, cada linha dessa table é um perfil, sempre há o perfil atual, com quantidade tal, e outros com outra quantidade. essa table sempre tem uma linha com as configs padrão.', -1),
@@ -31,7 +34,7 @@ insert into record (title, description, quantity) values
 ('v0.5.0', 'É possível utilizar IPFS e libp2p, diversos outras ferramentas pra auxiliar no processo de compartilhamento de cadastros, condições e transferências', -1),
 ('v0.5.0', 'Fazer com que as pessoas possam utilizar máquinas que tem controle para aliviar o tráfego em certos pontos da rede e permitir mais em geral. Não necessariamente as que elas possuem.', -1),
 
-('v0.6.0', 'Browser version, with dropdowns and buttons and pages and erethang'),
+('v0.6.0', 'Browser version, with dropdowns and buttons and pages and erethang', -1),
 ('v0.6.0', 'The registration can be done by typing, voice, photo or video, making it accessible and easy to use. For those without access to technology, it is possible to add their needs and contributions through any device or party.', -1),
 
 ('v0.7.0', 'Sugestão de Enriquecimento de cards, a pessoa coloca um prompt/foto do NI/CE e um MMLLM adiciona metadata, dps a pessoa confirma.', -1),
--- src/db/versions/db_dump.sql
+++ src/db/versions/db_dump.sql
@@ -133,14 +133,6 @@ ALTER TABLE ONLY public.record ALTER COLUMN id SET DEFAULT nextval('public.recor
 --
 
 COPY public.frequency (id, day_week, months, days, seconds, next_date, record_id, delta, times, finish_date, when_done) FROM stdin;
-2	1	0	0	0	2024-07-22 00:00:00+00	2	-1	1	\N	t
-3	2	0	0	0	2024-07-23 00:00:00+00	2	-2	1	\N	t
-4	3	0	0	0	2024-07-24 00:00:00+00	2	-10	1	\N	f
-5	\N	0	1	0	2024-08-15 00:00:00+00	2	-100	1	\N	f
-6	\N	0	1	0	2024-07-20 00:00:00+00	2	200	1	2024-07-19	f
-7	\N	0	10	0	2024-07-29 00:00:00+00	2	-1	1	2024-07-19	f
-8	\N	0	1	0	2024-07-19 14:16:30.913672+00	2	100	1	2024-07-10	f
-9	\N	0	-1	0	2024-07-18 00:00:00+00	2	-1	1	\N	t
 \.
 
 
@@ -149,7 +141,46 @@ COPY public.frequency (id, day_week, months, days, seconds, next_date, record_id
 --
 
 COPY public.record (id, title, description, location, quantity) FROM stdin;
-2	ir no super	\N	\N	-1
+42	v0.4.0	Checkpoint, i.e. "When a quantity reaches 4"	\N	-1
+44	v0.4.0	Proportion, i.e. "When a quantity changes a certain number"	\N	-1
+78	v0.3.0	Bug: when query select * from table <table> not returning rows	\N	-1
+63	v0.5.0	Transfer proposal and connection, i.e "A proposal of transfering a quantity from A to B, in return (or not) C receives some from D" "If you transfer an amount of apples to my apple registration, I will transfer so much money from my registration to yours. Contribution and Retribution (optional if it is a donation)."	\N	-1
+64	v0.5.0	Transferência múltipla, i.e "Entregar diversos itens por um só. Para comprar essa bota eu ofereço 20 reais e um candelabro".	\N	-1
+65	v0.5.0	Eventually consistent	\N	-1
+66	v0.5.0	Decentralization	\N	-1
+67	v0.5.0	Eventually consistent databases	\N	-1
+68	v0.5.0	pub/sub protocols, i.e. "Pessoas podem se inscrever com pub/sub com cada cadastro, referente ao assunto escolhido."	\N	-1
+69	v0.5.0	Merkle-CRDTs implementation	\N	-1
+70	v0.5.0	Authentication. # Check gajim for login inspiration.	\N	-1
+71	v0.5.0	É possível utilizar IPFS e libp2p, diversos outras ferramentas pra auxiliar no processo de compartilhamento de cadastros, condições e transferências	\N	-1
+72	v0.5.0	Fazer com que as pessoas possam utilizar máquinas que tem controle para aliviar o tráfego em certos pontos da rede e permitir mais em geral. Não necessariamente as que elas possuem.	\N	-1
+73	v0.6.0	Browser version, with dropdowns and buttons and pages and erethang	\N	-1
+74	v0.6.0	The registration can be done by typing, voice, photo or video, making it accessible and easy to use. For those without access to technology, it is possible to add their needs and contributions through any device or party.	\N	-1
+75	v0.7.0	Sugestão de Enriquecimento de cards, a pessoa coloca um prompt/foto do NI/CE e um MMLLM adiciona metadata, dps a pessoa confirma.	\N	-1
+76	v0.7.0	Algorithm and/or ML for optimization of transfer quotas and cost-efficient connections. By digitizing the information, Lince enables the use of optimization algorithms and machine learning for more effective planning of contributions. However, it is necessary to consider the human biases present in the algorithms and ensure transparency, consent and democracy when implementing any decision-making artificial intelligence. Lince Modelo/Template.	\N	-1
+52	v0.3.0	Templates: prov uma manifestação de uso. Pessoas vão modelar sua cadeia produtiva e mostrar ou não, as que mostrarem vão deixar pessoas copiar tudo. Depois o que sobra são trabalhadores se colocando nas tarefas pra levantar tal empreitada.	\N	-1
+53	v0.3.0	Copiar cadastro, seja seu ou de outrem: selecione a opção copiar coisa, e de qual table ai tu digita o where pra puxar o que tu quer editar, se resposta é sim em editar 1 por 1 abre 1 por 1 e passa por todas as colunas de todas as coisas, se é editar com where ele faz um bulk editing	\N	-1
+54	v0.3.0	Criar table config, vai conter as configurações do aplicativo, quando o aplicativo começar ele vai olhar todas as colunas e se comportar conforme as informações, cada linha dessa table é um perfil, sempre há o perfil atual, com quantidade tal, e outros com outra quantidade. essa table sempre tem uma linha com as configs padrão.	\N	-1
+55	v0.3.0	config save mode	\N	-1
+56	v0.3.0	config tables show at main view	\N	-1
+57	v0.3.0	config views	\N	-1
+58	v0.3.0	config menu options shown and their disposition on the screen and columns..	\N	-1
+59	v0.3.0	config sort	\N	-1
+60	v0.3.0	config filter on what tables	\N	-1
+61	v0.3.0	config view selected config options	\N	-1
+62	v0.3.0	config change architecture main. reads from config table and passes info as argument to functions	\N	-1
+77	v0.3.0	config truncation of certain columns in certain tables	\N	-1
+41	v0.4.0	Conditions (The objective is to have generalized conditions and consequences, so anything can trigger anything else.\\n A periodicity can run a script, a checkpoint can change a quantity through a proportion, etc.)	\N	-1
+43	v0.4.0	Rate, i.e. "When a quantity changes in a certain rate (change/time)"	\N	-1
+45	v0.4.0	Command, i.e. "Shell command, being able to trigger any script in any language, easy to do with nix-shells for dev envs"	\N	-1
+46	v0.4.0	Delta, i.e. "Set a quantity to more or less a number, -1, +4, etc."	\N	-1
+47	v0.4.0	Arquitetura de condições e consequências	\N	-1
+48	v0.4.0	Transform frequency into a building block, to be used just like any condition/consequence	\N	-1
+49	v0.4.0	Simulção do efeito do tempo nos cadastros, se possível a partir dos cadastros de todos, coletar esses dados e rodar uma função de pesquisa operacional de simulação, também funciona quando uma transferência foi confirmada mas não efetivada no mundo material, só prometida, aí vai ter o valor atual da quantidade -1 e o previsto caso tudo continue como setado.	\N	-1
+50	v0.4.0	make quantity zero with minimum types of keyboard	\N	-1
+51	v0.4.0	add help option with explanation of all options and columns info and defaults	\N	-1
+79	v0.3.0	Bug: capital F not activating SQL File option	\N	-1
+80	v0.3.0	Fix order of main functions so startup isnt janky, things look different after you go to the while loop, menu & stuff	\N	-1
 \.
 
 
@@ -164,7 +195,7 @@ SELECT pg_catalog.setval('public.frequency_id_seq', 9, true);
 -- Name: record_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.record_id_seq', 2, true);
+SELECT pg_catalog.setval('public.record_id_seq', 80, true);
 
 
 --

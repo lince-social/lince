60ea6cc0476f9a1aa198a347d5e5ce7dc7967995
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-08-09 00:44:13 -0300

    DB versioning, setting only 0.4.1 visible

--- src/db/versions/default.sql
+++ src/db/versions/default.sql
@@ -20,10 +20,6 @@ SET row_security = off;
 -- Data for Name: command; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.command VALUES (1, -1, 'touch test.rs');
-INSERT INTO public.command VALUES (2, 1, 'touch test.py');
-INSERT INTO public.command VALUES (3, 1, 'pdismc');
-INSERT INTO public.command VALUES (4, 1, 'python test.py');
 
 
 --
@@ -37,29 +33,31 @@ INSERT INTO public.configuration VALUES (1, 1, 'Automatic', 'SELECT * FROM recor
 -- Data for Name: frequency; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.frequency VALUES (17, 1, NULL, 0, 1, 0, '2024-08-01 19:56:56.08274+00', NULL);
-INSERT INTO public.frequency VALUES (18, 1, NULL, 0, 0, 2, '2024-08-01 04:06:18.374205+00', NULL);
 
 
 --
 -- Data for Name: record; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.record VALUES (73, 0, 'v0.6.0', 'Feature | Browser Version: 1. Dropdowns to choose. 2. Buttons. 3. Pages...', NULL);
-INSERT INTO public.record VALUES (74, 0, 'v0.8.0', 'Feature | Maximization of Value Architecture: Create an architecture that allows for any amount of receptables of models and rules that alter the functioning of the app.', NULL);
-INSERT INTO public.record VALUES (75, 0, 'v0.9.0', 'Feature | Acessibility: Get information through any medium and turn them into any operation (Any language, verbal, sign, whistled..).', NULL);
-INSERT INTO public.record VALUES (76, 0, 'v0.10.0', 'Feature | Optimization: get recommendations or automatically optimize all tables and execute actions. Make the text pretty and formated. The transfer proposals in accordance with social quotas, and cost/benefit, automatic transfer with highest one, Lince agent. Correct frequencies for records and commands. Script correction...', NULL);
-INSERT INTO public.record VALUES (49, -1, 'v0.5.0', 'Feature | Wheel of Time: simulate passage of time, to see the quantities changing. Be able see the result of a transfer. Tip: study Operational Research.', NULL);
-INSERT INTO public.record VALUES (63, -1, 'v0.5.0', 'Feature | Transfer: 1. Multiple parties make their proposal. Each part can receive and contribute many things to any party. 2. Every proposal is accepted. 3. Every party marks deal as uphold. Tip: study smart contracts.', NULL);
-INSERT INTO public.record VALUES (65, -1, 'v0.5.0', 'Feature | Eventually Consistent Databases.', NULL);
-INSERT INTO public.record VALUES (66, -1, 'v0.5.0', 'Feature | Decentralization: have different nodes of Lince that can communicate.', NULL);
-INSERT INTO public.record VALUES (68, -1, 'v0.5.0', 'Feature | Communication: between nodes, maybe use pub/sub protocol, IPFS, libp2p, Merkle-CRDTs.', NULL);
-INSERT INTO public.record VALUES (72, -1, 'v0.5.0', 'Feature | Computing Donation: Give the agent the option to make the machine use its resources for network traffic optimization. Whatever that means.', NULL);
-INSERT INTO public.record VALUES (70, -1, 'v0.5.0', 'Feature | Authentication. Tip: Check gajim for possible login inspiration.', NULL);
-INSERT INTO public.record VALUES (102, -1, 'v0.4.1', 'Enhancement| DB Versions: have different DBs, in a dir inside db, i.e. db/versions/, change postgre.sql to schema.sql, have an option to change loaded db or load default.sql', NULL);
-INSERT INTO public.record VALUES (103, -1, 'v0.4.1', 'Enhancement | Be able to change quantity of a record with a simple command, like 1q70 makes rq70 = 1, typying on the Your Choice:', NULL);
-INSERT INTO public.record VALUES (104, 1, 'v0.7.0', 'Feature | Streaming: be able to stream video and/or audio through a p2p connection. Be part of a karma expression.', NULL);
-INSERT INTO public.record VALUES (105, -1, 'v0.4.1', 'Enhancement | Karma Expression: crud any table/column.', NULL);
+INSERT INTO public.record VALUES (106, -1, 'v0.4.1', 'Enhancement | LIKE Expression: alter the quantity of any record that has body or header LIKE some part of the string, regex? postgres LIKE?', NULL);
+INSERT INTO public.record VALUES (73, 0, '', 'Feature | Browser Version: 1. Dropdowns to choose. 2. Buttons. 3. Pages...', NULL);
+INSERT INTO public.record VALUES (74, 0, '', 'Feature | Maximization of Value Architecture: Create an architecture that allows for any amount of receptables of models and rules that alter the functioning of the app.', NULL);
+INSERT INTO public.record VALUES (75, 0, '', 'Feature | Acessibility: Get information through any medium and turn them into any operation (Any language, verbal, sign, whistled..).', NULL);
+INSERT INTO public.record VALUES (76, 0, '', 'Feature | Optimization: get recommendations or automatically optimize all tables and execute actions. Make the text pretty and formated. The transfer proposals in accordance with social quotas, and cost/benefit, automatic transfer with highest one, Lince agent. Correct frequencies for records and commands. Script correction...', NULL);
+INSERT INTO public.record VALUES (49, 0, '', 'Feature | Wheel of Time: simulate passage of time, to see the quantities changing. Be able see the result of a transfer. Tip: study Operational Research.', NULL);
+INSERT INTO public.record VALUES (63, 0, '', 'Feature | Transfer: 1. Multiple parties make their proposal. Each part can receive and contribute many things to any party. 2. Every proposal is accepted. 3. Every party marks deal as uphold. Tip: study smart contracts.', NULL);
+INSERT INTO public.record VALUES (65, 0, '', 'Feature | Eventually Consistent Databases.', NULL);
+INSERT INTO public.record VALUES (66, 0, '', 'Feature | Decentralization: have different nodes of Lince that can communicate.', NULL);
+INSERT INTO public.record VALUES (68, 0, '', 'Feature | Communication: between nodes, maybe use pub/sub protocol, IPFS, libp2p, Merkle-CRDTs.', NULL);
+INSERT INTO public.record VALUES (72, 0, '', 'Feature | Computing Donation: Give the agent the option to make the machine use its resources for network traffic optimization. Whatever that means.', NULL);
+INSERT INTO public.record VALUES (70, 0, '', 'Feature | Authentication. Tip: Check gajim for possible login inspiration.', NULL);
+INSERT INTO public.record VALUES (102, 0, '', 'Enhancement| DB Versions: have different DBs, in a dir inside db, i.e. db/versions/, change postgre.sql to schema.sql, have an option to change loaded db or load default.sql', NULL);
+INSERT INTO public.record VALUES (104, 0, '', 'Feature | Streaming: be able to stream video and/or audio through a p2p connection. Be part of a karma expression.', NULL);
+INSERT INTO public.record VALUES (107, 0, '', 'Feature | Have the app know where your machine is located on the world', NULL);
+INSERT INTO public.record VALUES (108, 0, '', 'Feature | Default Location: When doing transfers the default location is where the machine is at the moment, only not when the location is filled with a coordinate.', NULL);
+INSERT INTO public.record VALUES (109, 1, '', 'Feature | Graph View: See dependent records and their triggers through karma, with its dependencies like commands and frequencies. Also view dependencies with other records from other nodes, see the chain, sypply chain.', NULL);
+INSERT INTO public.record VALUES (110, -1, 'v0.4.1', 'Feature | Views: select one or multiple views to see in one view', NULL);
+INSERT INTO public.record VALUES (105, -1, 'v0.4.1', 'Enhancement | Easy CRUD: be able to perform crud operations on any table through karma expressions and menu typying.', NULL);
 
 
 --
@@ -85,6 +83,21 @@ INSERT INTO public.history VALUES (142, 70, '2024-08-02 13:40:12.332949+00', -9,
 INSERT INTO public.history VALUES (144, 70, '2024-08-02 13:40:13.380425+00', -10, -11);
 INSERT INTO public.history VALUES (145, 70, '2024-08-02 13:40:45.751341+00', -11, -1);
 INSERT INTO public.history VALUES (146, 105, '2024-08-06 23:41:27.063145+00', 1, -1);
+INSERT INTO public.history VALUES (147, 49, '2024-08-09 02:10:20.704595+00', -1, 0);
+INSERT INTO public.history VALUES (148, 63, '2024-08-09 02:10:20.704595+00', -1, 0);
+INSERT INTO public.history VALUES (149, 65, '2024-08-09 02:10:20.704595+00', -1, 0);
+INSERT INTO public.history VALUES (150, 66, '2024-08-09 02:10:20.704595+00', -1, 0);
+INSERT INTO public.history VALUES (151, 68, '2024-08-09 02:10:20.704595+00', -1, 0);
+INSERT INTO public.history VALUES (152, 72, '2024-08-09 02:10:20.704595+00', -1, 0);
+INSERT INTO public.history VALUES (153, 70, '2024-08-09 02:10:20.704595+00', -1, 0);
+INSERT INTO public.history VALUES (154, 102, '2024-08-09 02:10:20.704595+00', -1, 0);
+INSERT INTO public.history VALUES (155, 104, '2024-08-09 02:10:20.704595+00', 1, 0);
+INSERT INTO public.history VALUES (156, 107, '2024-08-09 02:10:20.704595+00', 1, 0);
+INSERT INTO public.history VALUES (157, 108, '2024-08-09 02:10:20.704595+00', 1, 0);
+INSERT INTO public.history VALUES (159, 105, '2024-08-09 02:10:20.704595+00', -1, 0);
+INSERT INTO public.history VALUES (160, 106, '2024-08-09 02:10:20.704595+00', -1, 0);
+INSERT INTO public.history VALUES (162, 105, '2024-08-09 02:10:34.914652+00', 0, -1);
+INSERT INTO public.history VALUES (163, 106, '2024-08-09 02:10:34.914652+00', 0, -1);
 
 
 --
@@ -116,7 +129,7 @@ SELECT pg_catalog.setval('public.command_id_seq', 4, true);
 -- Name: configuration_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.configuration_id_seq', 1, false);
+SELECT pg_catalog.setval('public.configuration_id_seq', 3, true);
 
 
 --
@@ -130,7 +143,7 @@ SELECT pg_catalog.setval('public.frequency_id_seq', 18, true);
 -- Name: history_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.history_id_seq', 146, true);
+SELECT pg_catalog.setval('public.history_id_seq', 163, true);
 
 
 --
@@ -144,7 +157,7 @@ SELECT pg_catalog.setval('public.karma_id_seq', 30, true);
 -- Name: record_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.record_id_seq', 105, true);
+SELECT pg_catalog.setval('public.record_id_seq', 110, true);
 
 
 --

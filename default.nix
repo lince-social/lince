69479b156488ee6d97417d1a8d915a4b5562c604
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-08-13 18:56:52 -0300

    add npm packages

--- default.nix
+++ default.nix
@@ -3,8 +3,10 @@
 }:
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    postgresql
     git
+
+    postgresql
+
     python312
     python312Packages.python-lsp-server
     python312Packages.pandas
@@ -12,18 +14,20 @@ pkgs.mkShell {
     python312Packages.psycopg2
     python312Packages.tabulate
     python312Packages.urwid
+
+    # nodePackages.nodejs
+    nodejs_22
   ];
   PGDATA = "${toString ./.}/.pg";
   shellHook = ''
     pg_ctl stop
     export PGHOST="$PGDATA"
-
     [ ! -d $PGDATA ] && pg_ctl initdb -o "-U postgres"
     pg_ctl -o "-p 5432 -k $PGDATA" start
-
     echo "log_min_messages = warning" >> $PGDATA/postgresql.conf
     echo "log_checkpoints = off" >> $PGDATA/postgresql.conf
-    # pg_ctl restart
+
+    npm i @orbitdb/core helia @orbitdb/quickstart
 
     python src/terminal.py
   '';

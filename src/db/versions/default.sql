b7228a260db640c1546b61a9d1c0475aff63643f
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-08-16 21:14:03 +0000

    commit

--- src/db/versions/default.sql
+++ src/db/versions/default.sql
@@ -20,6 +20,8 @@ SET row_security = off;
 -- Data for Name: command; Type: TABLE DATA; Schema: public; Owner: -
 --
 
+INSERT INTO public.command VALUES (5, 1, 'python aaa.py');
+INSERT INTO public.command VALUES (6, 1, 'touch teste.md');
 
 
 --
@@ -34,7 +36,7 @@ INSERT INTO public.configuration VALUES (5, 1, 'Automatic', 2, 'verbose', '{}', 
 -- Data for Name: frequency; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.frequency VALUES (19, 1, NULL, 0, 1, 0, '2024-08-13 23:28:51.72614+00', NULL);
+INSERT INTO public.frequency VALUES (19, 1, NULL, 0, 1, 0, '2024-08-15 00:00:00+00', NULL);
 
 
 --
@@ -47,6 +49,54 @@ INSERT INTO public.history VALUES (168, 66, '2024-08-11 23:46:09.80722+00', 0, -
 INSERT INTO public.history VALUES (169, 70, '2024-08-11 23:46:09.80722+00', 0, -1);
 INSERT INTO public.history VALUES (170, 68, '2024-08-11 23:46:27.061796+00', 0, -1);
 INSERT INTO public.history VALUES (171, 70, '2024-08-12 23:29:47.623516+00', -1, -3);
+INSERT INTO public.history VALUES (172, 70, '2024-08-14 17:01:33.76144+00', -3, -4);
+INSERT INTO public.history VALUES (173, 70, '2024-08-14 17:01:34.460294+00', -4, -5);
+INSERT INTO public.history VALUES (174, 70, '2024-08-14 17:01:34.673296+00', -5, -6);
+INSERT INTO public.history VALUES (175, 70, '2024-08-14 17:01:34.867951+00', -6, -7);
+INSERT INTO public.history VALUES (176, 70, '2024-08-14 17:01:35.067403+00', -7, -8);
+INSERT INTO public.history VALUES (177, 70, '2024-08-14 17:01:35.264995+00', -8, -9);
+INSERT INTO public.history VALUES (178, 70, '2024-08-14 17:01:35.460727+00', -9, -10);
+INSERT INTO public.history VALUES (179, 70, '2024-08-14 17:01:35.656238+00', -10, -11);
+INSERT INTO public.history VALUES (180, 70, '2024-08-14 17:01:35.852752+00', -11, -12);
+INSERT INTO public.history VALUES (181, 70, '2024-08-14 17:01:36.046601+00', -12, -13);
+INSERT INTO public.history VALUES (182, 70, '2024-08-14 17:01:36.243952+00', -13, -14);
+INSERT INTO public.history VALUES (183, 70, '2024-08-14 17:01:36.442558+00', -14, -15);
+INSERT INTO public.history VALUES (184, 70, '2024-08-14 17:01:36.640776+00', -15, -16);
+INSERT INTO public.history VALUES (185, 70, '2024-08-14 17:01:36.83691+00', -16, -17);
+INSERT INTO public.history VALUES (186, 70, '2024-08-14 17:01:38.178574+00', -17, -18);
+INSERT INTO public.history VALUES (187, 70, '2024-08-14 17:02:00.676812+00', -18, -19);
+INSERT INTO public.history VALUES (188, 70, '2024-08-14 17:02:23.420752+00', -19, -20);
+INSERT INTO public.history VALUES (189, 70, '2024-08-14 17:02:26.950421+00', -20, -21);
+INSERT INTO public.history VALUES (190, 70, '2024-08-14 17:02:36.495295+00', -21, -22);
+INSERT INTO public.history VALUES (191, 70, '2024-08-14 17:02:44.463986+00', -22, -23);
+INSERT INTO public.history VALUES (192, 70, '2024-08-14 17:02:49.057927+00', -23, -24);
+INSERT INTO public.history VALUES (193, 70, '2024-08-14 17:03:01.942929+00', -24, -25);
+INSERT INTO public.history VALUES (194, 70, '2024-08-14 17:03:23.153572+00', -25, -26);
+INSERT INTO public.history VALUES (195, 70, '2024-08-14 17:03:24.015683+00', -26, -27);
+INSERT INTO public.history VALUES (196, 70, '2024-08-14 17:03:24.574725+00', -27, -28);
+INSERT INTO public.history VALUES (197, 70, '2024-08-14 17:03:25.070789+00', -28, -29);
+INSERT INTO public.history VALUES (198, 70, '2024-08-14 17:03:45.289471+00', -29, -30);
+INSERT INTO public.history VALUES (199, 70, '2024-08-14 17:04:19.192237+00', -30, -31);
+INSERT INTO public.history VALUES (200, 70, '2024-08-14 17:04:30.78703+00', -31, -32);
+INSERT INTO public.history VALUES (201, 70, '2024-08-14 17:04:39.399748+00', -32, -33);
+INSERT INTO public.history VALUES (202, 70, '2024-08-14 17:04:42.639026+00', -33, -34);
+INSERT INTO public.history VALUES (203, 70, '2024-08-14 17:05:13.81196+00', -34, -35);
+INSERT INTO public.history VALUES (204, 70, '2024-08-14 17:05:14.510132+00', -35, -36);
+INSERT INTO public.history VALUES (205, 70, '2024-08-14 17:05:15.223673+00', -36, -37);
+INSERT INTO public.history VALUES (206, 70, '2024-08-14 17:05:29.909423+00', -37, -38);
+INSERT INTO public.history VALUES (207, 70, '2024-08-14 17:06:00.74671+00', -38, -39);
+INSERT INTO public.history VALUES (208, 70, '2024-08-14 17:06:15.017588+00', -39, -3);
+INSERT INTO public.history VALUES (209, 70, '2024-08-14 17:06:15.032524+00', -3, -4);
+INSERT INTO public.history VALUES (210, 65, '2024-08-14 17:06:15.07914+00', -1, -58);
+INSERT INTO public.history VALUES (211, 70, '2024-08-14 17:06:26.37959+00', -4, -5);
+INSERT INTO public.history VALUES (212, 70, '2024-08-14 17:06:26.945987+00', -5, -6);
+INSERT INTO public.history VALUES (213, 70, '2024-08-14 17:06:27.214265+00', -6, -7);
+INSERT INTO public.history VALUES (214, 70, '2024-08-14 17:06:27.460134+00', -7, -8);
+INSERT INTO public.history VALUES (215, 70, '2024-08-14 17:06:27.701579+00', -8, -9);
+INSERT INTO public.history VALUES (216, 70, '2024-08-14 17:06:27.914302+00', -9, -10);
+INSERT INTO public.history VALUES (217, 70, '2024-08-14 17:07:36.473243+00', -10, -11);
+INSERT INTO public.history VALUES (218, 70, '2024-08-14 17:07:45.028077+00', -11, -12);
+INSERT INTO public.history VALUES (219, 70, '2024-08-14 17:07:51.246072+00', -12, -13);
 
 
 --
@@ -54,6 +104,10 @@ INSERT INTO public.history VALUES (171, 70, '2024-08-12 23:29:47.623516+00', -1,
 --
 
 INSERT INTO public.karma VALUES (31, 1, 'rq70 = f19 * -3');
+INSERT INTO public.karma VALUES (32, 1, 'rq70 = rq70 -1');
+INSERT INTO public.karma VALUES (33, 1, 'rq68 = c5');
+INSERT INTO public.karma VALUES (34, 1, 'rq65 = f19 * -58');
+INSERT INTO public.karma VALUES (35, 1, 'c6 = 1');
 
 
 --
@@ -72,9 +126,9 @@ INSERT INTO public.record VALUES (104, 0, '', 'Feature | Streaming: be able to s
 INSERT INTO public.record VALUES (107, 0, '', 'Feature | Have the app know where your machine is located on the world. Datatype: DEFAULT (59.880220, -43.732561)', NULL);
 INSERT INTO public.record VALUES (108, 0, '', 'Feature | Default Location: When doing transfers the default location is where the machine is at the moment, only not when the location is filled with a coordinate.', NULL);
 INSERT INTO public.record VALUES (109, 0, '', 'Feature | Graph View: See dependent records and their triggers through karma, with its dependencies like commands and frequencies. Also view dependencies with other records from other nodes, see the chain, sypply chain.', NULL);
-INSERT INTO public.record VALUES (65, -1, '', 'Feature | Eventually Consistent Databases.', NULL);
 INSERT INTO public.record VALUES (68, -1, '', 'Feature | Communication: between nodes, maybe use pub/sub protocol, IPFS, libp2p, Merkle-CRDTs.', NULL);
-INSERT INTO public.record VALUES (70, -3, '', 'Feature | Authentication. Tip: Check gajim for possible login inspiration.', NULL);
+INSERT INTO public.record VALUES (65, -58, '', 'Feature | Eventually Consistent Databases.', NULL);
+INSERT INTO public.record VALUES (70, -13, '', 'Feature | Authentication. Tip: Check gajim for possible login inspiration.', NULL);
 
 
 --
@@ -101,7 +155,7 @@ INSERT INTO public.views VALUES (2, 'SELECT * FROM record WHERE quantity < 0 ORD
 -- Name: command_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.command_id_seq', 4, true);
+SELECT pg_catalog.setval('public.command_id_seq', 6, true);
 
 
 --
@@ -122,14 +176,14 @@ SELECT pg_catalog.setval('public.frequency_id_seq', 19, true);
 -- Name: history_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.history_id_seq', 171, true);
+SELECT pg_catalog.setval('public.history_id_seq', 219, true);
 
 
 --
 -- Name: karma_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.karma_id_seq', 31, true);
+SELECT pg_catalog.setval('public.karma_id_seq', 35, true);
 
 
 --

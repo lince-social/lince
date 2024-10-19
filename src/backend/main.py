e04312424f4efde7d607c5c3a920e171fd1702d7
xaviduds <xaviduds@gmail.com>
2024-10-19 11:23:30 -0300

    commented print on timezone func

--- src/backend/main.py
+++ src/backend/main.py
@@ -322,13 +322,18 @@ def check_update_frequency(id):
 
     configuration_row = execute_sql_command('SELECT timezone FROM configuration ORDER BY quantity DESC LIMIT 1').iloc[0]
     configuration_timezone = configuration_row['timezone']
+    # print("timezone: " + configuration_timezone)
 
     tz_offset = timedelta(hours=int(configuration_timezone))
+    # print(f'tzoffset: {tz_offset}')
     tzinfo = timezone(tz_offset)
+    # print(f"tzinfo: {tzinfo}")
 
     next_date = frequency_row['next_date'].astimezone(tzinfo)
+    # next_date = frequency_row['next_date']
     time_now = datetime.now(tzinfo)
 
+    # print(f"1: {next_date} 2: {time_now}")
     if frequency_row['finish_date'] is not None and time_now.date() > frequency_row['finish_date']:
         return 0
 
@@ -339,6 +344,7 @@ def check_update_frequency(id):
         quantity = frequency_row['quantity'] + 1
         update_rows('frequency', set_clause=f"quantity = {quantity}", where_clause=f"id = {frequency_row['id']}")
 
+    # print(f": {}")
     next_date += relativedelta(months=int(frequency_row['months'])) + timedelta(days=int(frequency_row['days']), seconds=int(frequency_row['seconds']))
 
     if not pd.isna(frequency_row['day_week']):

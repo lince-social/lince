9a437745ac30ee025d5ae7ec08b3ae7780c5389a
xaviduds <xaviduds@gmail.com>
2024-11-22 18:38:01 -0300

    simplified default.nix and changed to npm

--- default.nix
+++ default.nix
@@ -3,23 +3,8 @@
 }:
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    git
-
     postgresql_16
-
-    python312
-    python312Packages.python-lsp-server
-    python312Packages.pandas
-    python312Packages.datetime
-    python312Packages.psycopg2
-    python312Packages.tabulate
-    python312Packages.flask
-
-    # nodePackages.nodejs
-    nodejs_22
-
-    graph-easy
-    slides
+    nodejs_23
   ];
   PGDATA = "${toString ./.}/.pg";
   shellHook = ''
@@ -30,15 +15,7 @@ pkgs.mkShell {
     echo "log_min_messages = warning" >> $PGDATA/postgresql.conf
     echo "log_checkpoints = off" >> $PGDATA/postgresql.conf
 
-    # [ $SCRIPT == 1 ] && slides ${toString ./documentation.md} || python ${toString ./src/flask_app.py}
-    if [ -z "$SCRIPT" ]; then
-        python ${toString ./src/flask_app.py}
-    elif [ "$SCRIPT" -eq 1 ]; then
-        python ${toString ./src/terminal.py}
-    elif [ "$SCRIPT" -eq 2 ]; then
-        slides ${toString ./documentation.md}
-    else
-        echo "Invalid SCRIPT value. Please use 1 for terminal.py, 2 for slides, or unset for flask_app.py."
-    fi
+    cd ${toString ./.}
+    npm run dev
   '';
 }

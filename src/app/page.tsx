ca3e793449cf87d8ad455eddb7c989652a242bbe
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-11-24 23:09:02 -0300

    moved old_lince to src/app and tinkered page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,62 +64,34 @@ export default function Home() {
   };
 
 return (
-  <div className="bg-indigo-950 p-2">
-  <header className="flex">
-    <div className="mb-4">
-      <button
-        onClick={clearAll}
-        className="px-4 py-2 bg-red-600 text-white border-none cursor-pointer"
-      >
-        Clear All
-      </button>
-    </div>
-    <div className="mb-4">
+  <div className="flex text-white">
+  <header className="flex flex-col fixed p-1 mt-1 ml-3">
+      <button onClick={clearAll} className="bg-slate-800 hover:bg-red-400 cursor-pointer">x</button>
       {views.length > 0 ? (
         views.map((view) => (
-          <button
-            key={view.id}
-            onClick={() => toggleView(view.id)}
-            className={`px-4 py-2 m-1 ${data[view.id] ? 'bg-blue-500' : 'bg-gray-300'} text-white border-none cursor-pointer`}
-          >
-            {view.view_name}
-          </button>
-        ))
-      ) : (
-        <p className="text-white">No views available</p>
-      )}
-    </div>
-    </header>
-
-
+          <button key={view.id} onClick={() => toggleView(view.id)}
+            className={`${data[view.id] ? 'bg-gray-700' : 'bg-gray-500'} hover:bg-slate-500 p-1 cursor-pointer`} >
+            {view.view_name} </button> )) ) : ( <p className="text-white">No views available</p> )}
+  </header>
+  <main className="flex-1 ml-[9%] mr-[1%] mt-2">
     {Object.values(loading).some((status) => status) && <p className="text-white">Loading...</p>}
 
     {!Object.values(loading).some((status) => status) && Object.keys(data).length > 0 && (
       <div>
         {Object.entries(data).map(([viewId, rows]) => (
-          <div key={viewId} className="mb-8">
-            <h2 className="text-xl font-semibold text-white">View {viewId}</h2>
+          <div key={viewId} className="mb-8 ">
+            <h2 className="text-xl font-semibold text-gray-700">View {viewId}</h2>
             {rows && rows.length > 0 ? (
-              <table className="w-full border-collapse mt-4">
+              <table className="table-auto w-full border-collapse mt-4">
                 <thead>
-                  <tr>
-                    {Object.keys(rows[0]).map((key) => (
-                      <th key={key} className="border border-gray-300 p-2 text-white">
-                        {key}
-                      </th>
-                    ))}
-                  </tr>
+                  <tr> {Object.keys(rows[0]).map((key, index) => (
+                      <th key={key} className={`border border-gray-300 p-2 text-white bg-gray-600 ${ index === Object.keys(rows[0]).length - 1 ? 'w-full' : 'whitespace-nowrap' }`} > {key} </th> ))} </tr>
                 </thead>
                 <tbody>
                   {rows.map((row, rowIndex) => (
-                    <tr key={rowIndex}>
-                      {Object.values(row).map((value, colIndex) => (
-                        <td key={colIndex} className="border border-gray-300 p-2 text-white">
-                          {String(value)}
-                        </td>
-                      ))}
-                    </tr>
-                  ))}
+                    <tr key={rowIndex}> {Object.values(row).map((value, colIndex) => (
+                        <td key={colIndex} className={`border border-gray-300 p-2 text-white bg-gray-500 ${ colIndex === Object.values(row).length - 1 ? 'w-full' : 'whitespace-nowrap' }`} > {String(value)} </td> ))}
+                    </tr> ))}
                 </tbody>
               </table>
             ) : (
@@ -129,5 +101,6 @@ return (
         ))}
       </div>
     )}
+    </main>
   </div>
 );

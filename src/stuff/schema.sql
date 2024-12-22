c930cc400d8bf9622ffa6f3a6e1c90810413177b
xaviduds <xaviduds@gmail.com>
2024-12-22 22:48:50 +0000

    refactored schema.sql

--- src/stuff/schema.sql
+++ src/stuff/schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE views (
 	id SERIAL PRIMARY KEY,
- 	view TEXT NOT NULL DEFAULT 'SELECT * FROM record',
-	view_name TEXT
+	view TEXT NOT NULL DEFAULT 'SELECT * FROM record',
+	view_name TEXT NOT NULL
 );
 
 CREATE TABLE configuration (
 	id SERIAL PRIMARY KEY,
 	quantity REAL NOT NULL DEFAULT 1,
 	save_mode VARCHAR(9) NOT NULL DEFAULT 'Automatic' CHECK (save_mode in ('Automatic', 'Manual')),
- 	view_id INTEGER NOT NULL DEFAULT 1,
+	view_id INTEGER NOT NULL DEFAULT 1,
 	column_information_mode VARCHAR(7) NOT NULL DEFAULT 'verbose' CHECK (column_information_mode in ('verbose', 'short', 'silent')),
 	keymap jsonb NOT NULL DEFAULT '{}',
 	language VARCHAR(15) NOT NULL DEFAULT 'en-US',
@@ -24,11 +24,11 @@ CREATE TABLE record (
 );
 
 CREATE TABLE history (
-    id SERIAL PRIMARY KEY,
-    record_id INTEGER NOT NULL,
-    change_time TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    old_quantity REAL NOT NULL,
-    new_quantity REAL NOT NULL
+	id SERIAL PRIMARY KEY,
+	record_id INTEGER NOT NULL,
+	change_time TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+	old_quantity REAL NOT NULL,
+	new_quantity REAL NOT NULL
 );
 
 CREATE TABLE karma (
@@ -53,11 +53,11 @@ CREATE TABLE sum (
 	record_id INTEGER NOT NULL,
 
 	sum_mode INTEGER NOT NULL DEFAULT 0 CHECK (sum_mode in (-1,0,1)),
-    interval_length INTERVAL NOT NULL,
-    interval_relative BOOL NOT NULL DEFAULT TRUE,
+	interval_length INTERVAL NOT NULL,
+	interval_relative BOOL NOT NULL DEFAULT TRUE,
 
 	end_lag interval,
-    end_date TIMESTAMP WITH TIME ZONE DEFAULT now()
+	end_date TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
 CREATE TABLE command (
@@ -77,20 +77,3 @@ CREATE TABLE transfer (
 	transfer_confirmation JSON,
 	transfer_time TIMESTAMP WITH TIME ZONE
 );
-
--- CREATE OR REPLACE FUNCTION record_quantity_change()
--- RETURNS TRIGGER AS $$
--- BEGIN
---     IF NEW.quantity IS DISTINCT FROM OLD.quantity THEN
---         INSERT INTO history (record_id, old_quantity, new_quantity)
---         VALUES (OLD.id, OLD.quantity, NEW.quantity);
---     END IF;
---     RETURN NEW;
--- END;
--- $$
---  LANGUAGE plpgsql;
-
--- CREATE TRIGGER record_quantity_update
--- AFTER UPDATE OF quantity ON record
--- FOR EACH ROW
--- EXECUTE FUNCTION record_quantity_change();

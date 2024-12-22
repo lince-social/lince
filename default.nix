0a1143d8dac67dde08c61d230d8e1e53a6b4d8ca
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-12-22 22:48:17 +0000

    revamped non-src files/dirs

--- default.nix
+++ default.nix
@@ -4,26 +4,26 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    postgresql_16
+    # postgresql_16
     nodejs_23
   ];
 
-  PGDATA = "${toString ./.}/.pg";
+  # PGDATA = "${toString ./.}/.pg";
 
   shellHook = ''
 
     # POSTGRES STUFF
-    pg_ctl stop
-    export PGHOST="$PGDATA"
-    [ ! -d $PGDATA ] && pg_ctl initdb -o "-U postgres"
-    pg_ctl -o "-p 5432 -k $PGDATA" start
-    echo "log_min_messages = warning" >> $PGDATA/postgresql.conf
-    echo "log_checkpoints = off" >> $PGDATA/postgresql.conf
+    # pg_ctl stop
+    # export PGHOST="$PGDATA"
+    # [ ! -d $PGDATA ] && pg_ctl initdb -o "-U postgres"
+    # pg_ctl -o "-p 5432 -k $PGDATA" start
+    # echo "log_min_messages = warning" >> $PGDATA/postgresql.conf
+    # echo "log_checkpoints = off" >> $PGDATA/postgresql.conf
 
     # NPM STUFF
     cd ${toString ./.}
-    npm i pg 
-    npm install --save-dev @types/pg
+    # npm i pg 
+    # npm install --save-dev @types/pg
     npm run dev
   '';
 }

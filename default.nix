ac40c21780a9c2928ab3133e67fa7a4c3c5b3a59
xaviduds <xaviduds@gmail.com>
2024-12-24 15:47:24 -0300

    uncommented postgres shellHook commands and updated postgresql to 17

--- default.nix
+++ default.nix
@@ -4,26 +4,21 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    # postgresql_16
+    postgresql_17
     nodejs_23
   ];
 
-  # PGDATA = "${toString ./.}/.pg";
+  PGDATA = "${toString ./.}/.pg";
 
   shellHook = ''
+    pg_ctl stop
+    export PGHOST="$PGDATA"
+    [ ! -d $PGDATA ] && pg_ctl initdb -o "-U postgres"
+    pg_ctl -o "-p 2000 -k $PGDATA" start
+    echo "log_min_messages = warning" >> $PGDATA/postgresql.conf
+    echo "log_checkpoints = off" >> $PGDATA/postgresql.conf
 
-    # POSTGRES STUFF
-    # pg_ctl stop
-    # export PGHOST="$PGDATA"
-    # [ ! -d $PGDATA ] && pg_ctl initdb -o "-U postgres"
-    # pg_ctl -o "-p 5432 -k $PGDATA" start
-    # echo "log_min_messages = warning" >> $PGDATA/postgresql.conf
-    # echo "log_checkpoints = off" >> $PGDATA/postgresql.conf
-
-    # NPM STUFF
     cd ${toString ./.}
-    # npm i pg 
-    # npm install --save-dev @types/pg
     npm run dev
   '';
 }

11a4fcf06855ada2b952d0a0db241c1c73f8d786
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-12-26 14:42:39 -0300

    revamp components for querying views

---
+++ src/components/Menu.tsx
@@ -0,0 +1,16 @@
+const options = [
+  ["App", "Operations", "Tables"],
+  ["[E] Exit", "[C] Create", "[0] Configuration"],
+  ["[H] Help", "[R] Read", "[1] History"],
+  ["[S] Save DB", "[U] Update", "[2] Record"],
+  ["[L] Load DB", "[D] Delete", "[3] Karma"],
+  ["[A] Activate Config", "[Q] Query", "[4] Frequency"],
+  ["", "[F] SQL File", "[5] Command"],
+  ["", "", "[6] Sum"],
+  ["", "", "[7] Transfer"],
+  ["", "", "[8] View"],
+];
+
+export default function Menu() {
+  return options.map((''))
+}

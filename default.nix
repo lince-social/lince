fa80e4f917918f912ee7b3b6ee38be8eeb93547d
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-12-26 14:38:36 -0300

    add prisma

--- default.nix
+++ default.nix
@@ -6,11 +6,21 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     postgresql_17
     nodejs_23
+    prisma-engines
+    prisma
   ];
 
   PGDATA = "${toString ./.}/.pg";
+  # PRISMA_MIGRATION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/migration-engine";
+  PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+  PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+  PRISMA_INTROSPECTION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/introspection-engine";
+  PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
 
   shellHook = ''
+    # PATH="$PWD/node_modules/.bin:$PATH";
+
+
     pg_ctl stop
     export PGHOST="$PGDATA"
     [ ! -d $PGDATA ] && pg_ctl initdb -o "-U postgres"
--- src/stuff/insert_if_empty.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO configuration (save_mode) SELECT ('Automatic') WHERE NOT EXISTS ( SELECT 1 FROM configuration);
-INSERT INTO views (view) SELECT ('SELECT * FROM record') WHERE NOT EXISTS ( SELECT 1 FROM views);

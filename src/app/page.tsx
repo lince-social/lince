83dadae0b6684f338c76ffe079f183e9a157c36a
Eduardo de Melo Xavier <xaviduds@gmail.com>
2025-01-23 19:11:14 -0300

    bunned it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Home from "@/components/section/Home";
+// import Home from "@/components/section/Home";
+
+import { sql } from "bun";
 
 export default function Page() {
   return (
     <>
-      <div>
-        <Home />
-      </div>
+      <div></div>
     </>
   );
 }
--- src/scripts/handleConfigurationChange.ts
+++ src/scripts/handleConfigurationChange.ts
@@ -1,5 +1,4 @@
 "use server";
-import { prisma } from "@lib/prisma";
 import { revalidatePath } from "next/cache";
 
 export default async function handleConfigurationClick(id) {

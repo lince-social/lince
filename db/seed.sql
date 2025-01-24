a4a00f703193289e5c988bc48b7c8b5e61efc11c
Eduardo de Melo Xavier <xaviduds@gmail.com>
2025-01-24 09:19:22 -0300

    New bun

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO record (head, quantity) VALUES ('Apple', -1);
+
+INSERT INTO view (viewName) VALUES ('Default View');
+
+INSERT INTO configuration (configurationName, views) VALUES ('Default Configuration', '{"Default View": true}');
+
--- src/scripts/startup.ts
+++ src/scripts/startup.ts
@@ -1,4 +1,4 @@
-import { sql, $ } from "bun";
+import { $, sql } from "bun";
 
 export async function deleteDatabaseIfExists() {
   await $`dropdb -h localhost -p 2000 -f --if-exists -U postgres --no-password newlince`;
@@ -10,33 +10,39 @@ export async function createDatabase() {
 
 export async function schemaDatabase() {
   await $`psql -U postgres -h localhost -p 2000 -d newlince < db/schema.sql`;
-  return console.log("schema database");
+}
+
+export async function grantPermissions() {
+  await $`psql -U postgres -h localhost -p 2000 -c "GRANT ALL PRIVILEGES ON DATABASE newlince TO postgres"`;
 }
 
 export async function checkEmptyDatabase() {
-  await sql`SELECT * FROM configuration`;
+  const configs = await sql`SELECT * FROM view;`;
+  return configs.length === 0;
 }
 
-export function saveDatabase() {
-  return console.log("save database");
+export async function loadDatabase() {
+  await $`psql -U postgres -h localhost -p 2000 -d newlince < ~/.config/lince/newlince.sql`;
 }
 
-export function loadDatabase() {
-  return console.log("load database");
+export async function seedDatabase() {
+  await $`psql -U postgres -h localhost -p 2000 -d newlince < db/seed.sql`;
 }
 
-export function seedDatabase() {
-  return console.log("seed database");
+export async function saveDatabase() {
+  await $`pg_dump -U postgres -h localhost -p 2000 -d newlince --no-owner --no-privileges --no-password --no-comments --data-only -F plain -f ~/.config/lince/newlince.sql`;
 }
 
 export async function main() {
   await deleteDatabaseIfExists();
   await createDatabase();
-  schemaDatabase();
-  // loadDatabase();
-  // if (checkEmptyDatabase() === true) {
-  //   seedDatabase();
-  // }
+  await schemaDatabase();
+  await grantPermissions();
+  await loadDatabase();
+  const isEmpty = await checkEmptyDatabase();
+  if (isEmpty) {
+    await seedDatabase();
+  }
 }
 
 main();

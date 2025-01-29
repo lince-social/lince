fda27ec4724ca5aad1b2413dbbb9230856e84948
xaviduds <xaviduds@gmail.com>
2025-01-29 21:21:48 +0000

    Crud and app changes

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
+import * as elements from "typed-html";
 import { Elysia } from "elysia";
 import { html } from "@elysiajs/html";
-import * as elements from "typed-html";
 
 import Page from "./components/sections/Page";
 import Body from "./components/sections/Body";
@@ -8,11 +8,18 @@ import ConfigurationsUnhovered, {
   ConfigurationsHovered,
 } from "./components/Configurations";
 import Tables from "./components/Tables";
-import handleConfigurationChange from "./utils/handleConfiguration";
-import { handleViewToggle } from "./utils/handleView";
-import HandleOperationInput from "./utils/handleOperation";
-import { sql } from "bun";
-import { RunQuery } from "./utils/crud";
+import {
+  ConfigurationChange,
+  CreateData,
+  DeleteView,
+  handleViewToggle,
+  RunQuery,
+  UpdateData,
+} from "./components//Crud";
+import OperationComponent from "./components/Operation";
+import ClosedTableOptions, {
+  OpenedTableOptions,
+} from "./components/TableOptions";
 
 export default async function app() {
   const app = new Elysia()
@@ -28,34 +35,18 @@ export default async function app() {
       );
     })
     .post("/configurationclick/:id", async ({ params: { id } }) => {
-      await handleConfigurationChange(id);
-      return await (
-        <main id="main">
-          <div>{await ConfigurationsHovered()}</div>
-          <div>{await Tables()}</div>
-        </main>
-      );
+      await ConfigurationChange(id);
     })
-    .post("/configurationhovered", async () => {
+    .get("/configurationhovered", async () => {
       return await ConfigurationsHovered();
     })
-    .post("/configurationunhovered", async () => {
+    .get("/configurationunhovered", async () => {
       return await ConfigurationsUnhovered();
     })
     .post("/view", async (context) => {
       const body = await context.body;
       const { views, view, configurationId } = body;
       await handleViewToggle(views, view, configurationId);
-      return await (
-        <main id="main">
-          <div>{await ConfigurationsHovered()}</div>
-          <div>{await Tables()}</div>
-        </main>
-      );
-    })
-    .delete("/view", async (context) => {
-      const body = await context.body;
-      console.log(body);
       return (
         <main id="main">
           <div>{await ConfigurationsHovered()}</div>
@@ -63,20 +54,32 @@ export default async function app() {
         </main>
       );
     })
+    .delete("/view", async ({ query }) => {
+      const { viewName, configurationId } = query;
+      return await DeleteView(viewName, configurationId);
+    })
 
     .post("/operation", async ({ body }) => {
       const { operation } = await body;
-      return await HandleOperationInput(operation);
+      return await OperationComponent(operation);
     })
     .post("/query", async ({ body }) => {
       const { query } = await body;
-      await sql(query);
+      return await RunQuery(query);
+    })
+    .post("/data", async ({ body }) => {
+      return await CreateData(body);
     })
     .put("/data", async ({ body }) => {
       const { table, set, where } = await body;
-      await RunQuery(`UPDATE ${table} SET ${set} WHERE ${where}`);
-      return Body();
+      return await UpdateData(table, set, where);
     })
+    .delete("/data", async ({ query }) => {
+      const { table, where } = query;
+      return await RunQuery(`DELETE FROM ${table} WHERE ${where}`);
+    })
+    .get("/openedtableoptions", await OpenedTableOptions())
+    .get("/closedtableoptions", await ClosedTableOptions())
     .listen(3000);
 
   console.log(`Serving at ${app.server?.hostname}:${app.server?.port}`);

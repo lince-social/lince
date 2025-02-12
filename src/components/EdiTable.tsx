f34700e4b64a06daf4e742f876393922491ee2d5
Eduardo de Melo Xavier <xaviduds@gmail.com>
2025-02-12 18:02:48 -0300

    remove comments & edit ' cells

--- src/components/EdiTable.tsx
+++ src/components/EdiTable.tsx
@@ -1,26 +1,7 @@
-// import * as elements from "typed-html";
-// export default async function DataNotFormComponent(table, id, key, value) {
-//   try {
-//     return (
-//       <td>
-//         <form hx-put={`/datanotform/${table}/${key}/${id}`} hx-target="#body">
-//           <input
-//             name="value"
-//             value={decodeURIComponent(value)}
-//             class="text-white bg-gray-600"
-//           />
-//         </form>
-//       </td>
-//     );
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
 import * as elements from "typed-html";
 
 export default async function DataNotFormComponent(table, id, key, value) {
   try {
-    // Escape single quotes for SQL
     const sanitizedValue = decodeURIComponent(value).replace(/'/g, "''");
 
     return (
--- src/components/Operation.tsx
+++ src/components/Operation.tsx
@@ -12,7 +12,7 @@ import {
 } from "./Crud";
 import { FatherBody } from "../components/sections/Body";
 
-export async function HandleOperation(operation) {
+export async function HandleOperation(operation: string) {
   let table;
 
   switch (true) {
@@ -102,96 +102,7 @@ export async function HandleOperation(operation) {
   }
 }
 
-// export async function HandleOperation(operation) {
-//   let table;
-//   switch (true) {
-//     case /0/.test(operation):
-//       table = "configuration";
-//       break;
-//     case /configuration/.test(operation):
-//       return await ReadDataComponent("configuration");
-//
-//     case /1/.test(operation):
-//       table = "history";
-//       break;
-//     case /history/.test(operation):
-//       return await ReadDataComponent("history");
-//
-//     case /2/.test(operation):
-//       table = "record";
-//       break;
-//     case /record/.test(operation):
-//       return await ReadDataComponent("record");
-//
-//     case /3/.test(operation):
-//       table = "karma";
-//       break;
-//     case /karma/.test(operation):
-//       return await ReadDataComponent("karma");
-//
-//     case /4/.test(operation):
-//       table = "frequency";
-//       break;
-//     case /frequency/.test(operation):
-//       return await ReadDataComponent("frequency");
-//
-//     case /5/.test(operation):
-//       table = "command";
-//       break;
-//     case /command/.test(operation):
-//       return await ReadDataComponent("command");
-//
-//     case /6/.test(operation):
-//       table = "sum";
-//       break;
-//     case /sum/.test(operation):
-//       return await ReadDataComponent("sum");
-//
-//     case /7/.test(operation):
-//       table = "transfer";
-//       break;
-//     case /transfer/.test(operation):
-//       return await ReadDataComponent("transfer");
-//
-//     case /8/.test(operation):
-//       table = "view";
-//       break;
-//     case /view/.test(operation):
-//       return await ReadDataComponent("view");
-//
-//     default:
-//       table = "record";
-//       break;
-//   }
-//
-//   switch (true) {
-//     case /c/.test(operation):
-//       return await CreateDataComponent(table);
-//     case /r/.test(operation):
-//       return await ReadDataComponent(table);
-//     case /u/.test(operation):
-//       return await UpdateDataComponent(table);
-//     case /d/.test(operation):
-//       return await DeleteDataComponent(table);
-//     case /s/.test(operation):
-//       saveDatabase();
-//       return;
-//     case /a/.test(operation):
-//       const id = operation.match(/\d+/)?.[0];
-//       if (typeof id !== "undefined") return ConfigurationChange(id);
-//       return;
-//     case /q/.test(operation):
-//       return await QueryInputComponent();
-//     case /f/.test(operation):
-//       return RunSqlFileComponent();
-//     case /h/.test(operation):
-//       return PrintHelpComponent();
-//     default:
-//       return ReadDataComponent(table);
-//   }
-// }
-
-export default async function OperationComponent(body) {
+export default async function OperationComponent(body: any) {
   const { operation } = await body;
   const HandledOperation = await HandleOperation(operation);
   return (

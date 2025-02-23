63d3f5ebcca5730beb93c1b8eb719e9b692c1c52
xaviduds <xaviduds@gmail.com>
2025-02-23 15:23:17 -0300

    divided src into dirs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
 fn main() {
-    let x = 3;
-    let mut s1 = String::from("hello");
-
-    if x >= 3 {
-        let foo = &mut s1;
-        foo.push_str(", word");
-        println!("{foo}")
-    } else {
-        let foo = &mut s1;
-        foo.push_str(", onsdoicdn");
-        println!("{foo}")
-    }
+    println!("hellowolrd")
 }
-
-// fn takeaway(s: &mut String) {
-//     s.push_str(", world");
-//     println!("{s}")
-// }

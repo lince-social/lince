c8869917dd43182a0f5c64bdea02524f05f2e6b8
Eduardo de Melo Xavier <xaviduds@gmail.com>
2025-06-04 23:05:17 -0300

    presentation changes

--- src/presentation/web/section/nav.rs
+++ src/presentation/web/section/nav.rs
@@ -1,5 +1,7 @@
 use crate::presentation::web::operation::get::presentation_web_operation_get_operation_input;
 
 pub async fn presentation_web_section_nav() -> String {
-    r#"<nav class="row">"#.to_string() + presentation_web_operation_get_operation_input() + "</nav>"
+    r#"<nav class="row">"#.to_string()
+        + presentation_web_operation_get_operation_input().as_str()
+        + "</nav>"
 }

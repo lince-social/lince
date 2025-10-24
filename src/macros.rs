2fb2f9d293c50bc069d2836f85b3011316d14156
Eduardo de Melo Xavier <xaviduds@gmail.com>
2025-10-23 22:41:40 -0300

    feat(KarmaView): editable

--- src/macros.rs
+++ src/macros.rs
@@ -17,6 +17,42 @@ macro_rules! ok {
         $expression.map_err(Error::other)?
     };
 }
+#[macro_export]
+macro_rules! htmx_edit_cell {
+    // default tag = td
+    ($table:expr, $id:expr, $field:expr, $value:expr) => {
+        $crate::htmx_edit_cell!($table, $id, $field, $value, td)
+    };
+    // explicit tag (e.g. div)
+    ($table:expr, $id:expr, $field:expr, $value:expr, $tag:ident) => {{
+        let value_str = $value.to_string();
+        let post_value = if value_str.is_empty() {
+            "NULL".to_string()
+        } else {
+            value_str.clone()
+        };
+        let display_value = if value_str.is_empty() {
+            "".to_string()
+        } else {
+            value_str.clone()
+        };
+        html! {
+            $tag {
+                form
+                    hx-post=(format!("/table/{}/{}/{}", $table, $id, $field))
+                    hx-swap="outerHTML"
+                    hx-target=(format!("closest {}", stringify!($tag)))
+                    hx-trigger="click"
+                {
+                    input type="hidden" name="value" value=(post_value) {}
+                    button type="submit" class="plain-button" {
+                        (display_value)
+                    }
+                }
+            }
+        }
+    }};
+}
 
 #[macro_export]
 macro_rules! query {

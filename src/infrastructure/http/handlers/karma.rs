d67de6d1ca33b1299b288931a4902d8b9d2759ed
Eduardo de Melo Xavier <xaviduds@gmail.com>
2025-10-29 23:10:26 -0300

    feat(Active Search): Done

--- src/infrastructure/http/handlers/karma.rs
+++ src/infrastructure/http/handlers/karma.rs
@@ -1,8 +1,8 @@
-use std::convert::Infallible;
-
 use crate::{
     infrastructure::cross_cutting::InjectedServices,
-    presentation::html::karma::search::presentation_html_karma_get_condition,
+    presentation::html::karma::search::{
+        presentation_html_karma_get_condition, presentation_html_karma_get_consequence,
+    },
 };
 use asynk_strim::{Yielder, stream_fn};
 use axum::{
@@ -12,6 +12,7 @@ use axum::{
 use datastar::prelude::PatchElements;
 use serde::Deserialize;
 use serde_json::from_str;
+use std::convert::Infallible;
 
 #[derive(Deserialize)]
 pub struct Wrapper {
@@ -64,6 +65,53 @@ pub async fn handler_karma_post_condition(
             yielder
                 .yield_item(Ok(PatchElements::new(
                     presentation_html_karma_get_condition(services, search).await,
+                )
+                .write_as_axum_sse_event()))
+                .await;
+        },
+    ))
+}
+
+pub async fn handler_karma_get_consequence(
+    State(services): State<InjectedServices>,
+    Query(wrapper): Query<Wrapper>,
+) -> impl IntoResponse {
+    let signals_value: serde_json::Value = match from_str(&wrapper.datastar) {
+        Ok(v) => v,
+        Err(e) => serde_json::Value::Null,
+    };
+
+    let search = signals_value
+        .get("search")
+        .and_then(|s| s.as_str())
+        .map(|s| s.to_string());
+
+    Sse::new(stream_fn(
+        move |mut yielder: Yielder<Result<Event, Infallible>>| async move {
+            yielder
+                .yield_item(Ok(PatchElements::new(
+                    presentation_html_karma_get_consequence(services, search).await,
+                )
+                .write_as_axum_sse_event()))
+                .await;
+        },
+    ))
+}
+
+pub async fn handler_karma_post_consequence(
+    State(services): State<InjectedServices>,
+    Json(body): Json<serde_json::Value>,
+) -> impl IntoResponse {
+    let search = body
+        .get("search")
+        .and_then(|s| s.as_str())
+        .map(|s| s.to_string());
+
+    Sse::new(stream_fn(
+        move |mut yielder: Yielder<Result<Event, Infallible>>| async move {
+            yielder
+                .yield_item(Ok(PatchElements::new(
+                    presentation_html_karma_get_consequence(services, search).await,
                 )
                 .write_as_axum_sse_event()))
                 .await;
